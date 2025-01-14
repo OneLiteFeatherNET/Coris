64efc17fff4324f2e38c0a8e7ebb27cd29468f53
theEvilReaper <steffenwx@gmail.com>
2025-01-14 12:54:26 +0100

    Add empty set as return type

--- src/main/java/net/theevilreaper/coris/util/DefaultPositionCalculator.java
+++ src/main/java/net/theevilreaper/coris/util/DefaultPositionCalculator.java
@@ -6,23 +6,24 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.Set;
 
 public class DefaultPositionCalculator {
 
     public static <T extends Point> Set<T> calculatePositions(T start, T end) {
-        return switch (start.getClass())
+        return Set.of();
     }
 
     private static Set<Vec> caluclateViaVec(@NotNull Vec start, @NotNull Vec end) {
-
+        return Collections.emptySet();
     }
 
     private static Set<Pos> caluclateViaPos(@NotNull Pos start, @NotNull Pos end) {
-
+        return Collections.emptySet();
     }
 
     private static Set<BlockVec> calculateViaPoint(@NotNull BlockVec start, @NotNull BlockVec end) {
-
+        return Collections.emptySet();
     }
 }
