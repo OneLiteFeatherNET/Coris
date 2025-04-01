9f15b7958267a73e24718c856995de53be9d80f0
theEvilReaper <steffenwx@gmail.com>
2025-04-01 12:21:24 +0200

    Add more code to represent a door

---
+++ src/main/java/net/theevilreaper/coris/door/AnimationState.java
@@ -0,0 +1,16 @@
+package net.theevilreaper.coris.door;
+
+/**
+ * The enumeration contains each animation state which a door can be in.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public enum AnimationState {
+
+    IDLE,
+    OPENING,
+    OPENED,
+    CLOSING,
+}
