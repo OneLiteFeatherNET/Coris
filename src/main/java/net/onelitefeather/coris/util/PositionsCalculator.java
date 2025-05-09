4b697e6821468ffa9504772538a025c460d70533
theEvilReaper <steffenwx@gmail.com>
2025-05-09 10:41:19 +0200

    Add documentation

--- src/main/java/net/onelitefeather/coris/util/PositionsCalculator.java
+++ src/main/java/net/onelitefeather/coris/util/PositionsCalculator.java
@@ -6,9 +6,24 @@ import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.Set;
 
+/**
+ * A functional interface for calculating positions between two points.
+ *
+ * @param <T> the type of point
+ * @version 1.0.0
+ * @since 0.1.0
+ * @author theEvilReaper
+ */
 @FunctionalInterface
 public interface PositionsCalculator<T extends Point> {
 
+    /**
+     * Calculates the positions between two points.
+     *
+     * @param start the start point
+     * @param end   the end point
+     * @return the positions between the two points
+     */
     @NotNull
     @UnmodifiableView
     Set<T> calculatePositions(@NotNull T start, @NotNull T end);
