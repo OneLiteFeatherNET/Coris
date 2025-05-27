7c0dfa596030e004d8ccdafd91497d3ea5e7669c
theEvilReaper <steffenwx@gmail.com>
2025-05-27 12:26:37 +0200

    Add TestComponent implementation

---
+++ src/test/java/net/onelitefeather/coris/util/TestComponent.java
@@ -0,0 +1,7 @@
+package net.onelitefeather.coris.util;
+
+import net.onelitefeather.coris.component.CorisComponent;
+import org.jetbrains.annotations.NotNull;
+
+public record TestComponent(@NotNull String data) implements CorisComponent {
+}
