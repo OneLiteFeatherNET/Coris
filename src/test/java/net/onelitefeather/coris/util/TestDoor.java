b3be5d40ed919a2d670577ede48acb1e63ff1878
theEvilReaper <steffenwx@gmail.com>
2025-05-27 10:02:19 +0200

    Add missing playAnimation method to the TestDoor

--- src/test/java/net/onelitefeather/coris/util/TestDoor.java
+++ src/test/java/net/onelitefeather/coris/util/TestDoor.java
@@ -2,6 +2,7 @@ package net.onelitefeather.coris.util;
 
 import net.kyori.adventure.key.Key;
 import net.minestom.server.coordinate.Point;
+import net.onelitefeather.coris.door.AnimationState;
 import net.onelitefeather.coris.door.BaseDoor;
 import net.onelitefeather.coris.door.DoorFace;
 import net.onelitefeather.coris.shape.Shape;
@@ -11,6 +12,11 @@ public final class TestDoor extends BaseDoor {
 
     public TestDoor(@NotNull Key key, @NotNull DoorFace face, @NotNull Shape<? extends Point> shape) {
         super(key, face, shape);
+    }
+
+    @Override
+    public void playAnimation(@NotNull AnimationState animationState) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
