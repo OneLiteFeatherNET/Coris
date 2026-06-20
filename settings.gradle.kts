6d14be9f38acd3718e6412641eb5c9131d9e6400
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-06-20 10:27:43 +0200

    chore(deps): update version and mycelium bom for this specific release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("bom", "1.7.1")
+            version("bom", "1.6.7")
 
             library("mycelium.bom", "net.onelitefeather", "mycelium-bom").versionRef("bom")
 
